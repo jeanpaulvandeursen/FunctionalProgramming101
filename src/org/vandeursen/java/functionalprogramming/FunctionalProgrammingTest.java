6d5bf1b6fc4163d9329e42e1715e1d00e62fe219
Jean-Paul van Deursen <jpvd@Jean-Pauls-MacBook-Pro-2.local>
2019-02-08 16:41:31 +0100

    added 6th prime

--- src/org/vandeursen/java/functionalprogramming/FunctionalProgrammingTest.java
+++ src/org/vandeursen/java/functionalprogramming/FunctionalProgrammingTest.java
@@ -13,6 +13,7 @@ public class FunctionalProgrammingTest {
         System.out.println(isPrime(3));
         System.out.println(isPrime(4));
         System.out.println(isPrime(5));
+        System.out.println(isPrime(6));
 
         //Find the double of the first even number greater than 3
         List<Integer> values = Arrays.asList(1,2,3,5,4,6,7,8,9,10);
